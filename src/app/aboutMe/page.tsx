1b4461e435291262a606ce7d507c7960307b53ed
MerezkaYurii <shkolasad@ukr.net>
2025-08-20 18:33:17 +0300

    fix2

--- src/app/aboutMe/page.tsx
+++ src/app/aboutMe/page.tsx
@@ -1,5 +1,15 @@
 import React from 'react';
 
 export default function AboutMePage() {
-  return <div>pageAboutMe</div>;
+  return (
+    <div>
+      pageAboutMe
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur
+        aspernatur reiciendis error, illo vitae expedita. Molestias, numquam
+        placeat iusto minus itaque, excepturi ullam delectus maxime quibusdam
+        ipsa, optio officiis ratione!
+      </p>
+    </div>
+  );
 }
