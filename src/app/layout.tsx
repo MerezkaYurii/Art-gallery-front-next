9504adceb1d815f8d53f390a43e9ec2b3615226a
MerezkaYurii <shkolasad@ukr.net>
2025-09-07 18:14:36 +0300

    fix meta 1

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,9 +6,34 @@ import KeyboardManager from './components/KeyboardManager';
 
 export const metadata: Metadata = {
   title: 'Art Gallery',
-  description: 'Описание сайта',
+  description:
+    'Картини художниці Світлани Точинської — світ емоцій та натхнення.',
   icons: {
-    icon: '/pictureForHead.jpg',
+    icon: '/previous-up.jpg',
+  },
+  openGraph: {
+    title: 'Art Gallery',
+    description:
+      'Картини художниці Світлани Точинської — світ емоцій та натхнення.',
+    url: 'https://art-gallery-front-next-one.vercel.app',
+    siteName: 'Art Gallery',
+    images: [
+      {
+        url: '/previous.jpg',
+        width: 1200,
+        height: 630,
+        alt: 'Art Gallery preview',
+      },
+    ],
+    locale: 'en_US',
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Art Gallery',
+    description:
+      'Картини художниці Світлани Точинської — світ емоцій та натхнення.',
+    images: ['/previous.jpg'],
   },
 };
 
